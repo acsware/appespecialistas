c4b6c0e1af7af0940e2e9d46d7f875ee9cb05ca9
Antonio Cala <39869943+acsware@users.noreply.github.com>
2018-06-10 19:51:28 -0500

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,25 +1,29 @@
 <?php
-header("content-type: text/html;charset='utf-8'")
-$nombre = htmlentities($_POST['nombres']);
-$email = htmlentities($_POST['email']);
-$telefono = htmlentities($_POST['telefono']);
-$textomensaje = htmlentities($_POST['textomensaje']);
-$direccion_ip = $_SERVER['REMOTE_ADDR'];
-
-$header = 'From: '.$email." \n";
-$header.= "X-Mailer: PHP/".phpversion()." \n";
-$header.= "content-type: text/plain";
-$mensaje = "Este mensaje fue enviado" .$nombre." \n";
-$mensaje .= "Su email es".$email." \n";
-$mensaje .= "Su mensaje es".$textomensaje." \n";
-$mensaje .= "La direccion IP del Mensaje es:" .$direccion_ip ." \n";
-$para = "anjca@hotmail.com";
-$asunto = "Mensaje Enviado atraves del Formulario de la Pagina Consultorio de Especialistas";
-if(mail($para,$asunto,utf8_decode($mensaje),$header))
+header("content-type: text/html;charset='utf-8'");
+if($_POST['email'])
 {
-	echo "Mensaje enviado Gracias";
-}
-else{
-	echo "Mensaje No enviado, Verifique el Formulario";
-}
+    $nombre = htmlentities($_POST['nombres']);
+    $email = htmlentities($_POST['email']);
+    $telefono = htmlentities($_POST['telefono']);
+    $textomensaje = htmlentities($_POST['textomensaje']);
+    $direccion_ip = $_SERVER['REMOTE_ADDR'];
+    
+    $header = 'From: '.$email." \n";
+    $header.= "X-Mailer: PHP/".phpversion()." \n";
+    $header.= "content-type: text/plain";
+    $mensaje = "Este mensaje fue enviado por: " .$nombre." \n";
+    $mensaje .= "Su email es: ".$email." \n";
+    $mensaje .= "Su mensaje es: ".$textomensaje." \n";
+    $mensaje .= "La direccion IP del Remitente es: " .$direccion_ip ." \n";
+    $para = "anjca@hotmail.com";
+    $asunto = "Mensaje Enviado desde el Consultorio de Especialistas de:".$email;
+    if(mail($para,$asunto,utf8_decode($mensaje),$header))
+    {
+    	echo "<h1>Mensaje Enviado</h1><br><a href='javascript:history.back(1)'>Volver Atras</a>";
+    }
+    else{
+    	echo "<h1>Informacion No valida, Verifique los datos de Origen</h1><br><a href='javascript:history.back(1)'>Volver Atras</a>";
+    }
+}else
+echo ("<h1>Informacion No valida, Verifique los datos de Origen</h1><br><a href='javascript:history.back(1)'>Volver Atras</a>");
 ?>
